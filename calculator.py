fdb994e27d57ded0f9c96fd8984f2c63be42c58a
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2020-07-08 11:31:31 +0530

    Added: Show memory and show history windows and its functions

--- calculator.py
+++ calculator.py
@@ -10,6 +10,7 @@ def plus_memory():
         memory = str(int(result))
     else:
         memory = str(result)
+    memory_store.append("M+ " + number)
     is_memory_used = True
     is_memory_clicked = True
 
@@ -22,6 +23,7 @@ def minus_memory():
         memory = str(int(result))
     else:
         memory = str(result)
+    memory_store.append("M-  " + number)
     is_memory_used = True
     is_memory_clicked = True
 
@@ -36,9 +38,10 @@ def recall_memory():
 
 
 def clear_memory():
-    global memory, is_memory_used
+    global memory, memory_store, is_memory_used
     memory = "0"
     is_memory_used = False
+    memory_store = []
 
 
 def number_click(value):
@@ -84,10 +87,12 @@ def one_by_x_click():
     global number, is_calculation_complete
     if number == "0":
         display_value.set("Cannot divide by zero")
+        history.append("¹/" + number + " = " + "Cannot divide by zero")
         is_calculation_complete = True
     else:
         value = float(number)
         value = 1 / value
+        history.append("¹/" + number + " = " + str(value))
         integer = value.is_integer()
         if integer:
             number = str(int(value))
@@ -118,6 +123,7 @@ def square_click():
     global number, is_calculation_complete
     value = float(number)
     value *= value
+    history.append(number + "² = " + str(value))
     integer = value.is_integer()
     if integer:
         number = str(int(value))
@@ -130,6 +136,7 @@ def square_click():
 def root_click():
     global number, is_calculation_complete
     result = math.sqrt(float(number))
+    history.append("√" + number + " = " + str(result))
     integer = result.is_integer()
     if integer:
         number = str(int(result))
@@ -146,6 +153,7 @@ def cube_root():
         result = value ** (1. / 3.)
     else:
         result = -(-value) ** (1. / 3.)
+    history.append("∛" + number + " = " + str(result))
     integer = result.is_integer()
     if integer:
         number = str(int(result))
@@ -158,6 +166,7 @@ def cube_root():
 def ten_raise_to():
     global number, is_calculation_complete
     result = float(number) ** 10
+    history.append(number + "^10" + " = " + str(result))
     integer = result.is_integer()
     if integer:
         number = str(int(result))
@@ -179,6 +188,14 @@ def percentage():
         elif operator == "/":
             result = float(old_number) / float(number)
         result = result / 100
+        if operator == "+":
+            history.append("(" + old_number + "+" + number + ") % = " + str(result))
+        elif operator == "-":
+            history.append("(" + old_number + "-" + number + ") % = " + str(result))
+        elif operator == "*":
+            history.append("(" + old_number + "x" + number + ") % = " + str(result))
+        elif operator == "/":
+            history.append("(" + old_number + "/" + number + ") % = " + str(result))
         integer = result.is_integer()
         if integer:
             number = str(int(result))
@@ -245,11 +262,12 @@ def delete():
 
 
 def clear():
-    global number, operator, old_number, is_calculate_init, is_operator_clicked, is_dot_clicked, is_calculation_complete, on_start
+    global number, operator, old_number, is_calculate_init, is_operator_clicked, is_memory_clicked, is_dot_clicked, is_calculation_complete, on_start
     is_calculate_init = False
     is_calculation_complete = False
     is_dot_clicked = False
     is_operator_clicked = False
+    is_memory_clicked = False
     on_start = True
     operator = "+"
     old_number = "0"
@@ -268,20 +286,27 @@ def perform_operation(first_number, second_number, operate_with):
     global number, is_zero_division_error
     if operate_with == "+":
         result = float(first_number) + float(second_number)
+        history.append(first_number + "+" + second_number + " = " + str(result))
     elif operate_with == "-":
         result = float(first_number) - float(second_number)
+        history.append(first_number + "-" + second_number + " = " + str(result))
     elif operate_with == '*':
         result = float(first_number) * float(second_number)
+        history.append(first_number + "x" + second_number + " = " + str(result))
     elif operate_with == "/":
         if int(second_number) == 0:
             display_value.set("Cannot divide by zero")
+            history.append(first_number + "/" + second_number + " = " + "Cannot divide by zero")
             is_zero_division_error = True
         else:
             result = float(first_number) / float(second_number)
+            history.append(first_number + "/" + second_number + " = " + str(result))
     elif operate_with == "^":
         result = float(first_number) ** float(second_number)
+        history.append(first_number + "^" + second_number + " = " + str(result))
     elif operate_with == "hyp":
-        trignometric_operations(first_number, second_number, operate_with)
+        result = math.hypot(float(first_number), float(second_number))
+        history.append("hyp (" + first_number + "," + second_number + ") = " + str(result))
     if is_zero_division_error:
         is_zero_division_error = False
     else:
@@ -294,25 +319,30 @@ def perform_operation(first_number, second_number, operate_with):
         number = str(to_display)
 
 
-def trignometric_operations(first_number, second_number, operate_with):
+def trignometric_operations(second_number, operate_with):
     global number, is_calculation_complete, is_dot_clicked
     value = float(second_number)
     if operate_with == "sin":
         result = math.sin(math.radians(value))
+        history.append("Sin (" + second_number + ") = " + str(result))
     elif operate_with == "cos":
         result = math.cos(math.radians(value))
+        history.append("Cos (" + second_number + ") = " + str(result))
     elif operate_with == "tan":
         result = math.tan(math.radians(value))
+        history.append("Tan (" + second_number + ") = " + str(result))
     elif operate_with == "sec":
-        result = 1/(math.cos(math.radians(value)))
+        result = 1 / (math.cos(math.radians(value)))
+        history.append("Sec (" + second_number + ") = " + str(result))
     elif operate_with == "csc":
         result = 1 / (math.sin(math.radians(value)))
+        history.append("Csc (" + second_number + ") = " + str(result))
     elif operate_with == "cot":
-        result = 1/(math.tan(math.radians(value)))
+        result = 1 / (math.tan(math.radians(value)))
+        history.append("Cot (" + second_number + ") = " + str(result))
     elif operate_with == "rad":
         result = math.radians(value)
-    elif operate_with == "hyp":
-        result = math.hypot(float(first_number), float(second_number))
+        history.append("rad (" + second_number + ") = " + str(result))
     integer = result.is_integer()
     if integer:
         number = str(int(result))
@@ -381,12 +411,66 @@ def scientific():
     cot_button.grid(row=3, column=2, padx=(0, 0), pady=(0, 0), sticky="nsew")
     percent_button.grid(row=3, column=3, padx=(15, 0), pady=(0, 0), sticky="nsew")
     ten_raise_to_x_button.grid(row=3, column=4, padx=(0, 15), pady=(0, 0), sticky="nsew")
+    bottom_buttons = [zero_button, plus_button, minus_button]
+    dot_button.grid(pady=(15, 15))
+    equal_button.grid(pady=(15, 15))
+    for i in bottom_buttons:
+        i.grid(pady=(0, 15))
+
+
+def show_history():
+    history_window = Toplevel(window)
+    history_window.title("History")
+    history_window.configure(bg="#000")
+    history_window.iconphoto(False, photo)
+    history_window.geometry("500x310+585+10")
+    scrollbar = Scrollbar(history_window)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    history_list = Listbox(history_window, bg="#000", fg="#fff", width=31, height=8, font=('arial', 20, 'bold'))
+    history_list.pack(padx=(15, 15), pady=(15, 15))
+
+    if history == []:
+        history_list.insert(END, "Nothing to show")
+    for i in history:
+        history_list.insert(END, " " + i)
+
+    history_list.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=history_list.yview)
+
+    history_window.resizable(0, 0)
+    history_window.mainloop()
+
+
+def show_memory():
+    memory_window = Toplevel(window)
+    memory_window.title("History")
+    memory_window.configure(bg="#000")
+    memory_window.iconphoto(False, photo)
+    memory_window.geometry("500x310+585+380")
+    scrollbar = Scrollbar(memory_window)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    memory_list = Listbox(memory_window, bg="#000", fg="#fff", width=31, height=8, font=('arial', 20, 'bold'))
+    memory_list.pack(padx=(15, 15), pady=(15, 15))
+
+    if memory_store == []:
+        memory_list.insert(END, "Nothing to show")
+    for i in memory_store:
+        memory_list.insert(END, " " + i)
+
+    memory_list.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=memory_list.yview)
+
+    memory_window.resizable(0, 0)
+    memory_window.mainloop()
 
 
 # Setting Up Calculator Window
 window = Tk()
 window.title("Calculator")
 window.configure(bg="#000")
+window.geometry("+5+10")
 # Setting Up Window Icon
 photo = PhotoImage(file="icons/icon.png")
 window.iconphoto(False, photo)
@@ -409,6 +493,8 @@ display_value.set("0")
 on_start = True
 theme_var = IntVar()
 view_var = IntVar()
+history = []
+memory_store = []
 
 # Creating Menu
 menu_bar = Menu(window)
@@ -418,8 +504,8 @@ file = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='File', menu=file)
 show_menu = Menu(file, tearoff=0)
 file.add_cascade(label='Show ', menu=show_menu)
-show_menu.add_command(label='Show History', command=None)
-show_menu.add_command(label='Show Memory', command=None)
+show_menu.add_command(label='Show History', command=lambda: show_history())
+show_menu.add_command(label='Show Memory', command=lambda: show_memory())
 theme_menu = Menu(file, tearoff=0)
 file.add_cascade(label='Theme ', menu=theme_menu)
 theme_menu.add_radiobutton(label='Light', value=0, variable=theme_var, command=lambda: light_theme())
@@ -441,72 +527,73 @@ display = Entry(window, font=('arial', 30, 'bold'), textvariable=display_value, 
 # Setting up Calculator Buttons
 # First Row
 memory_clear_button = Button(window, width=5, height=1, bg="#018729", fg="#fff", activebackground="#01942d",
-                             activeforeground="#fff", font=('arial', 20, 'bold'), text="MC",
+                             activeforeground="#fff", font=('arial', 15, 'bold'), text="MC",
                              command=lambda: clear_memory())
 memory_clear_button.grid(row=1, column=0, padx=(15, 0), pady=(15, 0), sticky="nsew")
 memory_recall_button = Button(window, width=5, height=1, bg="#018729", fg="#fff", activebackground="#01942d",
-                              activeforeground="#fff", font=('arial', 20, 'bold'), text="MR",
+                              activeforeground="#fff", font=('arial', 15, 'bold'), text="MR",
                               command=lambda: recall_memory())
 memory_recall_button.grid(row=1, column=1, padx=(0, 0), pady=(15, 0), sticky="nsew")
 memory_plus_button = Button(window, width=5, height=1, bg="#018729", fg="#fff", activebackground="#01942d",
-                            activeforeground="#fff", font=('arial', 20, 'bold'), text="M+",
+                            activeforeground="#fff", font=('arial', 15, 'bold'), text="M+",
                             command=lambda: plus_memory())
 memory_plus_button.grid(row=1, column=2, padx=(0, 0), pady=(15, 0), sticky="nsew")
 memory_minus_button = Button(window, width=5, height=1, bg="#018729", fg="#fff", activebackground="#01942d",
-                             activeforeground="#fff", font=('arial', 20, 'bold'), text="M-",
+                             activeforeground="#fff", font=('arial', 15, 'bold'), text="M-",
                              command=lambda: minus_memory())
 memory_minus_button.grid(row=1, column=3, padx=(0, 15), pady=(15, 0), sticky="nsew")
 delete_button = Button(window, width=5, height=1, bg="#ff6f00", fg="#fff", activebackground="#fa8100",
-                       font=('arial', 20, 'bold'), text="⌫", command=lambda: delete())
+                       font=('arial', 15, 'bold'), text="⌫", command=lambda: delete())
 delete_button.grid(row=1, column=4, padx=(0, 15), pady=(15, 0), sticky="nsew")
 
 # Second Row
 sin_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Sin",
-                    command=lambda: trignometric_operations(old_number, number, "sin"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Sin",
+                    command=lambda: trignometric_operations(number, "sin"))
 cos_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Cos",
-                    command=lambda: trignometric_operations(old_number, number, "cos"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Cos",
+                    command=lambda: trignometric_operations(number, "cos"))
 tan_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Tan",
-                    command=lambda: trignometric_operations(old_number, number, "tan"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Tan",
+                    command=lambda: trignometric_operations(number, "tan"))
 hyp_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="hyp",
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="hyp",
                     command=lambda: operator_click("hyp"))
 rad_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    font=('arial', 20, 'bold'), text="rad", command=lambda: trignometric_operations(old_number, number, "rad"))
+                    font=('arial', 15, 'bold'), text="rad",
+                    command=lambda: trignometric_operations(number, "rad"))
 
 # Third Row
 sec_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Sec",
-                    command=lambda: trignometric_operations(old_number, number, "sec"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Sec",
+                    command=lambda: trignometric_operations(number, "sec"))
 csc_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Cse",
-                    command=lambda: trignometric_operations(old_number, number, "csc"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Cse",
+                    command=lambda: trignometric_operations(number, "csc"))
 cot_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                    activeforeground="#fff", font=('arial', 20, 'bold'), text="Cot",
-                    command=lambda: trignometric_operations(old_number, number, "cot"))
+                    activeforeground="#fff", font=('arial', 15, 'bold'), text="Cot",
+                    command=lambda: trignometric_operations(number, "cot"))
 percent_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                        activeforeground="#fff", font=('arial', 20, 'bold'), text="%",
+                        activeforeground="#fff", font=('arial', 15, 'bold'), text="%",
                         command=lambda: percentage())
 ten_raise_to_x_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                               font=('arial', 20, 'bold'), text="10ˣ", command=lambda: ten_raise_to())
+                               font=('arial', 15, 'bold'), text="10ˣ", command=lambda: ten_raise_to())
 
 # Fourth Row
 one_by_x_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                         font=('arial', 20, 'bold'), text="¹/x", command=lambda: one_by_x_click())
+                         font=('arial', 15, 'bold'), text="¹/x", command=lambda: one_by_x_click())
 one_by_x_button.grid(row=4, column=0, padx=(15, 0), pady=(15, 0), sticky="nsew")
 square_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                       font=('arial', 20, 'bold'), text="x²", command=lambda: square_click())
+                       font=('arial', 15, 'bold'), text="x²", command=lambda: square_click())
 square_button.grid(row=4, column=1, padx=(0, 0), pady=(15, 0), sticky="nsew")
 raise_to_button = Button(window, width=5, height=1, bg="#0052cc", fg="#fff", activebackground="#006cfa",
-                         font=('arial', 20, 'bold'), text="xʸ", command=lambda: operator_click("^"))
+                         font=('arial', 15, 'bold'), text="xʸ", command=lambda: operator_click("^"))
 raise_to_button.grid(row=4, column=2, padx=(0, 0), pady=(15, 0), sticky="nsew")
 clear_entry_button = Button(window, width=5, height=1, bg="#ff6f00", fg="#fff", activebackground="#fa8100",
-                            font=('arial', 20, 'bold'), text="CE", command=lambda: clear_entry())
+                            font=('arial', 15, 'bold'), text="CE", command=lambda: clear_entry())
 clear_entry_button.grid(row=4, column=3, padx=(15, 0), pady=(15, 0), sticky="nsew")
 clear_button = Button(window, width=5, height=1, bg="#ff6f00", fg="#fff", activebackground="#fa8100",
-                      font=('arial', 20, 'bold'), text="C", command=lambda: clear())
+                      font=('arial', 15, 'bold'), text="C", command=lambda: clear())
 clear_button.grid(row=4, column=4, padx=(0, 15), pady=(15, 0), sticky="nsew")
 
 # Fifth Row
